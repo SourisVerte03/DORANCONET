b1f16c45c7b8f763d205a367233c62de91bcb65c
Theo Mousnier <mousniert.pro@gmail.com>
2024-11-21 12:05:08 +0100

    create Set and Get UserModel

---
+++ models/UserModel.php
@@ -0,0 +1,156 @@
+<?php
+class UserModel{
+    public $id;
+    public $firstName;
+    public $lastName;
+    public $date_of_birth;
+    public $email;
+    public $profile_picture;
+    public $password;
+    public $is_active;
+    public $deleted_at;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateOfBirth()
+    {
+        return $this->date_of_birth;
+    }
+
+    /**
+     * @param mixed $date_of_birth
+     */
+    public function setDateOfBirth($date_of_birth): void
+    {
+        $this->date_of_birth = $date_of_birth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfilePicture()
+    {
+        return $this->profile_picture;
+    }
+
+    /**
+     * @param mixed $profile_picture
+     */
+    public function setProfilePicture($profile_picture): void
+    {
+        $this->profile_picture = $profile_picture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsActive()
+    {
+        return $this->is_active;
+    }
+    /**
+     * @param mixed $is_active
+     */
+    public function setIsActive($is_active): void
+    {
+        $this->is_active = $is_active;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeletedAt()
+    {
+        return $this->deleted_at;
+    }
+
+    /**
+     * @param mixed $deleted_at
+     */
+    public function setDeletedAt($deleted_at): void
+    {
+        $this->deleted_at = $deleted_at;
+    }
+
+}
